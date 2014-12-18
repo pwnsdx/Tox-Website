8972a18d14f5a9a3943d6615fbdeb91c974a4e22
Sabri <sabri@mykolab.com>
2014-12-18 23:42:31 +0100

    Working on presentation

--- src/assets/js/tox.bootstrap.js
+++ src/assets/js/tox.bootstrap.js
@@ -144,6 +144,26 @@
             {
                 // Get right background density and load it
                 Tox.utils.injectBackground('aside.tObject section.preview', 'aside.tObject section.preview div');
+                
+                // Create Tween
+                var Tween = TweenMax.to([
+                    'aside.tObject section.preview',
+                    'aside.tObject section.preview div'
+                ], 1, {
+                    className: '+=tween',
+                    ease: Sine.easeInOut
+                });
+                
+                // Create the new scene
+                var scrollScenes = [
+                    (new ScrollScene(
+                    {
+                        duration: 1200
+                    })
+                    //.setPin('aside.tObject section.preview')
+                    .setTween(Tween)
+                    .addTo(Tox.d.scrollInstance))
+                ];
             }
         },
         
